cec716af729e11c06ab86a29001c8f6384dec229
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-02 17:00:04 +0300

    create initial Navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <div>
+        <a href="/">My Blog</a>
+      </div>
+      <div>
+        <a href="/">Home</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
