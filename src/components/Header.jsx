8740edd3eea7b5f3f70519d56f9605cdf2ccb572
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-03 01:08:07 +0300

    create initial Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Personal Blog</h1>
+      <p>Reflections, thoughts and opinions of mine</p>
+    </header>
+  );
+};
+
+export default Header;
