9a94f72dece6eed3ba9a866a9d58a3800298bccb
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-02 13:28:35 +0300

    add a customer color theme for the application

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "chetwode-blue": "#7469B6",
+        "east-side": "#AD88C6",
+        "french-lilac": "#E1AFD1",
+        "misty-rose": "#FFE6E6",
+        "white-smoke": "#ECECEC",
+        nero: "#2C2C2C",
+      },
+    },
   },
   plugins: [],
 };
