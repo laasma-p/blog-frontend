a0c607c13ffbdbe42de8f151a87c1c0046d4122f
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-04 00:50:17 +0300

    retrieve and display post from location state

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Post = () => {
+  const location = useLocation();
+  const post = location.state;
+
   return (
     <div className="bg-white-smoke h-dvh">
       <div className="max-w-4xl mx-auto p-6 text-nero">
@@ -10,11 +13,11 @@ const Post = () => {
         >
           ← Back to posts
         </Link>
-        <h1 className="text-4xl font-bold mb-4">Post title</h1>
+        <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
         <span className="mb-6">
-          <em>Written on date</em>
+          <em>Written on {post.date}</em>
         </span>
-        <p className="text-lg">Post content will be here</p>
+        <p className="text-lg">{post.content}</p>
       </div>
     </div>
   );
