ac54138559a569d9c87e9a38f0cf8cae61f3cb0d
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-04 00:13:56 +0300

    style Post component with TailwindCSS

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,16 @@
 const Post = () => {
   return (
-    <div>
-      <button>← Back to posts</button>
-      <h1>Post title</h1>
-      <p>
-        <em>Written on date</em>
-      </p>
-      <p>Post content will be her</p>
+    <div className="bg-white-smoke h-dvh">
+      <div className="max-w-4xl mx-auto p-6 text-nero">
+        <button className="hover:text-chetwode-blue transition-colors duration-300">
+          ← Back to posts
+        </button>
+        <h1 className="text-4xl font-bold mb-4">Post title</h1>
+        <span className="mb-6">
+          <em>Written on date</em>
+        </span>
+        <p className="text-lg">Post content will be here</p>
+      </div>
     </div>
   );
 };
