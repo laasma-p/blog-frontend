0dfb6e3ff85205e62b4809b72a9508bdacd3d7b8
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-03 18:14:06 +0300

    add a route for individual post

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
+import Post from "./components/Post";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/post/:postId" element={<Post />} />
       </Routes>
     </>
   );
