1067214e75dfbd2cf59faedf2511cd2395437b9f
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-03 18:12:24 +0300

    create initial individual Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,14 @@
+const Post = () => {
+  return (
+    <div>
+      <button>← Back to posts</button>
+      <h1>Post title</h1>
+      <p>
+        <em>Written on date</em>
+      </p>
+      <p>Post content will be her</p>
+    </div>
+  );
+};
+
+export default Post;
