ce55ee24d117a9672e08bebd395ad1709adc985d
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-05 10:27:27 +0300

    render Register component as a route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 import Post from "./components/Post";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/post/:postId" element={<Post />} />
       </Routes>
     </>
