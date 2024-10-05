bb4e961ef21d781784647112b453a8d1111004fb
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-05 19:08:51 +0300

    style forms with TailwindCSS

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,35 @@
 const Login = () => {
   return (
-    <div>
-      <div>
-        <h2>Login</h2>
-        <form>
+    <div className="bg-white-smoke min-h-screen flex justify-center">
+      <div className="max-w-md w-full p-6 text-nero">
+        <h2 className="text-3xl pb-4 font-semibold">Login</h2>
+        <form className="space-y-4">
           <div>
-            <label htmlFor="email">E-mail</label>
-            <input type="email" name="email" id="email" />
+            <label htmlFor="email" className="block font-medium">
+              E-mail
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="mt-1 p-2 w-full border border-chetwode-blue rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-east-side"
+              required
+            />
           </div>
           <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+            <label htmlFor="password" className="block font-medium">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="mt-1 p-2 w-full border border-chetwode-blue rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-east-side"
+            />
           </div>
-          <button>Login</button>
+          <button className="w-full py-2 bg-chetwode-blue text-white-smoke rounded-md shadow-md hover:bg-east-side hover:text-nero transition-colors duration-300">
+            Login
+          </button>
         </form>
       </div>
     </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,22 +1,48 @@
 const Register = () => {
   return (
-    <div>
-      <div>
-        <h2>Register</h2>
-        <form>
+    <div className="bg-white-smoke min-h-screen flex justify-center">
+      <div className="max-w-md w-full p-6 text-nero">
+        <h2 className="text-3xl pb-4 font-semibold">Register</h2>
+        <form className="space-y-4">
           <div>
-            <label htmlFor="firstName">First name</label>
-            <input type="text" name="firstName" id="firstName" />
+            <label htmlFor="firstName" className="block font-medium">
+              First name
+            </label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              className="mt-1 p-2 w-full border border-chetwode-blue rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-east-side"
+              required
+            />
           </div>
           <div>
-            <label htmlFor="email">E-mail</label>
-            <input type="email" name="email" id="email" />
+            <label htmlFor="email" className="block font-medium">
+              E-mail
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="mt-1 p-2 w-full border border-chetwode-blue rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-east-side"
+              required
+            />
           </div>
           <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+            <label htmlFor="password" className="block font-medium">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="mt-1 p-2 w-full border border-chetwode-blue rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-east-side"
+              required
+            />
           </div>
-          <button>Register</button>
+          <button className="w-full py-2 bg-chetwode-blue text-white-smoke rounded-md shadow-md hover:bg-east-side hover:text-nero transition-colors duration-300">
+            Register
+          </button>
         </form>
       </div>
     </div>
