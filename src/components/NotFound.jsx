5295125937ec934c86de33443a9fe99bddb1a1b0
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-06 10:32:39 +0300

    style NotFound component with TailwindCSS

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,12 +1,17 @@
 const NotFound = () => {
   return (
-    <div>
-      <h2>404 Not Found</h2>
-      <p>
+    <div className="flex flex-col items-center min-h-screen bg-white-smoke text-center">
+      <h2 className="text-3xl font-bold text-nero mt-56 mb-4">404 Not Found</h2>
+      <p className="text-lg text-nero mb-4">
         The content you are looking for either does not exist or has been
         removed.
       </p>
-      <a>Go back to home</a>
+      <a
+        href="#"
+        className="text-lg text-chetwode-blue hover:text-east-side transition-colors duration-300"
+      >
+        Go back to home
+      </a>
     </div>
   );
 };
