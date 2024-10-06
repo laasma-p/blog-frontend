45756c542a2ed8f7576eea102dfe6c846207943c
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-06 10:17:50 +0300

    create initial NotFound component

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h2>404 Not Found</h2>
+      <p>
+        The content you are looking for either does not exist or has been
+        removed.
+      </p>
+      <a>Go back to home</a>
+    </div>
+  );
+};
+
+export default NotFound;
