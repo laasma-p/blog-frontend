c126b005b8401a4eedfd8de6498bd338eac8b066
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-06 10:20:53 +0300

    render NotFound component as a route to point to nonexistant content when applicable

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Post from "./components/Post";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/post/:postId" element={<Post />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
