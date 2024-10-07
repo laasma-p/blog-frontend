32361d42d36e06a84203d5581117a74886721b88
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-08 00:13:08 +0300

    conditionally render NotFound component content based on authentication state

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,4 +1,6 @@
-const NotFound = () => {
+import { Link } from "react-router-dom";
+
+const NotFound = ({ isAuthenticated }) => {
   return (
     <div className="flex flex-col items-center min-h-screen bg-white-smoke text-center">
       <h2 className="text-3xl font-bold text-nero mt-56 mb-4">404 Not Found</h2>
@@ -6,12 +8,21 @@ const NotFound = () => {
         The content you are looking for either does not exist or has been
         removed.
       </p>
-      <a
-        href="#"
-        className="text-lg text-chetwode-blue hover:text-east-side transition-colors duration-300"
-      >
-        Go back to home
-      </a>
+      {isAuthenticated ? (
+        <Link
+          to="/dashboard"
+          className="text-lg text-chetwode-blue hover:text-east-side transition-colors duration-300"
+        >
+          Go back to dashboard
+        </Link>
+      ) : (
+        <Link
+          to="/"
+          className="text-lg text-chetwode-blue hover:text-east-side transition-colors duration-300"
+        >
+          Go back to home
+        </Link>
+      )}
     </div>
   );
 };
