84f93db1a10a03b1eaf6d467d981aaca65a65b25
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-08 00:22:38 +0300

    conditionally render Navigation component content based on authentication state

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,36 @@
 import { Link, NavLink } from "react-router-dom";
 
-const Navigation = () => {
+const Navigation = ({ isAuthenticated }) => {
   return (
     <nav className="flex justify-between items-center p-4 bg-chetwode-blue shadow-lg text-nero">
       <Link to="/" className="text-2xl font-bold">
         My Blog
       </Link>
-      <NavLink
-        to="/"
-        className="hover:text-white-smoke transition-colors duration-300"
-      >
-        Home
-      </NavLink>
+      <div className="flex space-x-4">
+        {isAuthenticated ? (
+          <>
+            <NavLink
+              to="/dashboard"
+              className="hover:text-white-smoke transition-colors duration-300"
+            >
+              Dashboard
+            </NavLink>
+            <NavLink
+              to="/add-a-post"
+              className="hover:text-white-smoke transition-colors duration-300"
+            >
+              Add A Post
+            </NavLink>
+          </>
+        ) : (
+          <NavLink
+            to="/"
+            className="hover:text-white-smoke transition-colors duration-300"
+          >
+            Home
+          </NavLink>
+        )}
+      </div>
     </nav>
   );
 };
