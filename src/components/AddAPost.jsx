1d00c8d71263dc41f50c548a016546650aaefb4b
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-26 16:21:23 +0200

    adjust styling for both Markdown preview and the form

--- src/components/AddAPost.jsx
+++ src/components/AddAPost.jsx
@@ -47,77 +47,83 @@ const AddAPost = () => {
   };
 
   return (
-    <div className="container mx-auto bg-white-smoke min-h-screen py-8 px-8">
-      <h2 className="text-4xl font-semibold text-nero mb-6 text-center">
-        Add a new post
-      </h2>
-      <form
-        onSubmit={addAPostHandler}
-        className="bg-white-smoke p-8 rounded-lg shadow-lg max-w-3xl mx-auto"
-      >
-        <div className="mb-6">
-          <label
-            htmlFor="title"
-            className="block font-medium text-nero text-lg mb-2"
+    <div className="bg-white-smoke min-h-screen">
+      <div className="container mx-auto py-8 px-8 flex flex-col lg:flex-row gap-2">
+        <div className="w-full lg:w-1/2 p-4 border border-chetwode-blue">
+          <h2 className="text-4xl font-semibold text-nero mb-6 text-center">
+            Add a new post
+          </h2>
+          <form
+            onSubmit={addAPostHandler}
+            className="bg-white-smoke p-4 rounded-lg shadow-lg mx-auto"
           >
-            Title
-          </label>
-          <input
-            type="text"
-            id="title"
-            className="w-full p-2 border border-chetwode-blue rounded-md focus:outline-none focus:ring-2 focus:ring-east-side"
-            value={title}
-            onChange={titleChangeHandler}
-            required
-            placeholder="Enter the title of the post"
-          />
+            <div className="mb-6">
+              <label
+                htmlFor="title"
+                className="block font-medium text-nero text-lg mb-2"
+              >
+                Title
+              </label>
+              <input
+                type="text"
+                id="title"
+                className="w-full p-2 border border-chetwode-blue rounded-md focus:outline-none focus:ring-2 focus:ring-east-side"
+                value={title}
+                onChange={titleChangeHandler}
+                required
+                placeholder="Enter the title of the post"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="content"
+                className="block font-medium text-nero text-lg mb-2"
+              >
+                Content
+              </label>
+              <textarea
+                id="content"
+                className="w-full p-2 border border-chetwode-blue rounded-md focus:outline-none focus:ring-2 focus:ring-east-side"
+                value={content}
+                onChange={contentChangeHandler}
+                required
+                placeholder="Write what is on your mind here"
+                rows="5"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="status"
+                className="block font-medium text-nero text-lg mb-2"
+              >
+                Status
+              </label>
+              <select
+                id="status"
+                className="w-full p-2 border border-chetwode-blue rounded-md focus:outline-none focus:ring-2 focus:ring-east-side"
+                value={status}
+                onChange={statusChangeHandler}
+              >
+                <option value="published">Publish</option>
+                <option value="draft">Save as draft</option>
+              </select>
+            </div>
+            <button
+              type="submit"
+              className="w-full py-2 bg-chetwode-blue text-white-smoke rounded-md shadow-md hover:bg-east-side hover:text-nero transition-colors duration-300"
+            >
+              Save post
+            </button>
+          </form>
         </div>
-        <div className="mb-6">
-          <label
-            htmlFor="content"
-            className="block font-medium text-nero text-lg mb-2"
-          >
-            Content
-          </label>
-          <textarea
-            id="content"
-            className="w-full p-2 border border-chetwode-blue rounded-md focus:outline-none focus:ring-2 focus:ring-east-side"
-            value={content}
-            onChange={contentChangeHandler}
-            required
-            placeholder="Write what is on your mind here"
-            rows="5"
-          />
+        <div className="w-full lg:w-1/2 mt-8 lg:mt-0 p-4 border border-chetwode-blue">
+          <h2 className="text-4xl font-semibold text-nero mb-6 text-center">
+            Post preview
+          </h2>
+          <div className="bg-white-smoke p-4 rounded-lg shadow-md break-all">
+            <ReactMarkdown>{content}</ReactMarkdown>
+          </div>
         </div>
-        <div className="mb-6">
-          <label
-            htmlFor="status"
-            className="block font-medium text-nero text-lg mb-2"
-          >
-            Status
-          </label>
-          <select
-            id="status"
-            className="w-full p-2 border border-chetwode-blue rounded-md focus:outline-none focus:ring-2 focus:ring-east-side"
-            value={status}
-            onChange={statusChangeHandler}
-          >
-            <option value="published">Publish</option>
-            <option value="draft">Save as draft</option>
-          </select>
-        </div>
-        <button
-          type="submit"
-          className="w-full py-2 bg-chetwode-blue text-white-smoke rounded-md shadow-md hover:bg-east-side hover:text-nero transition-colors duration-300"
-        >
-          Save post
-        </button>
-      </form>
-      <div className="mt-8">
-        <h3 className="text-3xl font-semibold text-nero mb-6 text-center">
-          Post preview
-        </h3>
-        <ReactMarkdown>{content}</ReactMarkdown>
       </div>
     </div>
   );
