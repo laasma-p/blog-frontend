d83fe387359f70dd930e1f9342ef2f63f9f844b0
Lasma Poksane <laasmapoksane@gmail.com>
2024-10-28 11:07:08 +0100

    adjust styling of Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,41 +37,58 @@ const Login = ({ setIsAuthenticated }) => {
   };
 
   return (
-    <div className="bg-white-smoke min-h-screen flex justify-center">
-      <div className="max-w-md w-full p-6 text-nero">
-        <h2 className="text-3xl pb-4 font-semibold">Login</h2>
-        <form className="space-y-4" onSubmit={loggingInHandler}>
-          <div>
-            <label htmlFor="email" className="block font-medium">
-              E-mail
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="mt-1 p-2 w-full border border-chetwode-blue rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-east-side"
-              required
-              value={email}
-              onChange={emailChangeHandler}
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="block font-medium">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className="mt-1 p-2 w-full border border-chetwode-blue rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-east-side"
-              value={password}
-              onChange={passwordChangeHandler}
-            />
-          </div>
-          <button className="w-full py-2 bg-chetwode-blue text-white-smoke rounded-md shadow-md hover:bg-east-side hover:text-nero transition-colors duration-300">
+    <div className="bg-white-smoke min-h-screen">
+      <div className="container mx-auto py-8 px-4 flex flex-col">
+        <div
+          className="p-4 w-full border-border-chetwode-blue
+           max-w-xl mx-auto"
+        >
+          <h2 className="text-4xl font-semibold text-nero mb-6 text-center">
             Login
-          </button>
-        </form>
+          </h2>
+          <form
+            onSubmit={loggingInHandler}
+            className="bg-white-smoke p-4 rounded-lg shadow-lg mx-auto"
+          >
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block font-medium text-nero text-lg mb-2"
+              >
+                E-mail
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="p-2 w-full border border-chetwode-blue rounded-md focus:outline-none focus:ring-2 focus:ring-east-side"
+                required
+                value={email}
+                onChange={emailChangeHandler}
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block font-medium text-nero text-lg mb-2"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="p-2 w-full border border-chetwode-blue rounded-md focus:outline-none focus:ring-2 focus:ring-east-side"
+                required
+                value={password}
+                onChange={passwordChangeHandler}
+              />
+            </div>
+            <button className="w-full py-2 bg-chetwode-blue text-white-smoke rounded-md shadow-md hover:bg-east-side hover:text-nero transition-colors duration-300">
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
