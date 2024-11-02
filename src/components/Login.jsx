d5ac1f6c11e375907c9324bd5695d04ffe6a6cf0
Lasma Poksane <laasmapoksane@gmail.com>
2024-11-02 15:44:39 +0100

    add initial user feedback and validation to Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,43 +5,81 @@ const Login = ({ setIsAuthenticated }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [validationErrors, setValidationErrors] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+
   const navigate = useNavigate();
 
   const emailChangeHandler = (event) => {
     setEmail(event.target.value);
+    setValidationErrors((prevErrors) => {
+      return { ...prevErrors, email: null };
+    });
   };
 
   const passwordChangeHandler = (event) => {
     setPassword(event.target.value);
+    setValidationErrors((prevErrors) => {
+      return { ...prevErrors, password: null };
+    });
+  };
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (!email) {
+      errors.email = "Email is required.";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = "Email format is invalid.";
+    }
+
+    if (!password) {
+      errors.password = "Password is required.";
+    }
+
+    return errors;
   };
 
   const loggingInHandler = async (event) => {
     event.preventDefault();
     setLoading(true);
+    setErrorMessage("");
 
-    const response = await fetch("http://localhost:3000/api/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
+    const errors = validateForm();
 
-    const data = await response.json();
+    if (Object.keys(errors).length > 0) {
+      setValidationErrors(errors);
+      setLoading(false);
+      return;
+    }
 
-    if (response.ok) {
-      const expirationTime = Date.now() + 3600000;
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
 
-      localStorage.setItem("token", data.token);
-      localStorage.setItem("expirationTime", expirationTime);
+      const data = await response.json();
 
-      setIsAuthenticated(true);
-      navigate("/dashboard");
-    } else {
-      console.error("Cannot log in.");
+      if (!response.ok) {
+        setErrorMessage(data.message);
+      } else {
+        const expirationTime = Date.now() + 3600000;
+
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("expirationTime", expirationTime);
+
+        setIsAuthenticated(true);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      setErrorMessage(data.message);
+    } finally {
       setLoading(false);
     }
-    setLoading(false);
   };
 
   return (
@@ -51,6 +89,9 @@ const Login = ({ setIsAuthenticated }) => {
           <h2 className="text-4xl font-semibold text-nero mb-6 text-center">
             Login
           </h2>
+          {errorMessage && (
+            <p className="text-amaranth text-md text-center">{errorMessage}</p>
+          )}
           <form
             onSubmit={loggingInHandler}
             className="bg-white-smoke p-4 rounded-lg shadow-lg mx-auto"
@@ -66,11 +107,17 @@ const Login = ({ setIsAuthenticated }) => {
                 type="email"
                 name="email"
                 id="email"
-                className="p-2 w-full border border-chetwode-blue rounded-md focus:outline-none focus:ring-2 focus:ring-east-side"
-                required
+                className={`p-2 w-full border rounded-md focus:outline-none focus:ring-2 ${
+                  validationErrors.email
+                    ? "border-amaranth focus:ring-amaranth"
+                    : "border-chetwode-blue focus:ring-chetwode-blue"
+                }`}
                 value={email}
                 onChange={emailChangeHandler}
               />
+              {validationErrors.email && (
+                <p className="text-amaranth">{validationErrors.email}</p>
+              )}
             </div>
             <div className="mb-6">
               <label
@@ -83,11 +130,17 @@ const Login = ({ setIsAuthenticated }) => {
                 type="password"
                 name="password"
                 id="password"
-                className="p-2 w-full border border-chetwode-blue rounded-md focus:outline-none focus:ring-2 focus:ring-east-side"
-                required
+                className={`p-2 w-full border rounded-md focus:outline-none focus:ring-2 ${
+                  validationErrors.password
+                    ? "border-amaranth focus:ring-amaranth"
+                    : "border-chetwode-blue focus:ring-chetwode-blue"
+                }`}
                 value={password}
                 onChange={passwordChangeHandler}
               />
+              {validationErrors.password && (
+                <p className="text-amaranth">{validationErrors.password}</p>
+              )}
             </div>
             <button
               className={`w-full py-2 bg-chetwode-blue text-white-smoke rounded-md shadow-md transition-colors duration-300 ${
