016437672d94925d115c567390db8a700caecac5
Lasma Poksane <laasmapoksane@gmail.com>
2024-11-06 02:00:08 +0100

    adjust color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,9 @@ export default {
     extend: {
       colors: {
         "dark-purple": "#3B1E54",
+        "bright-purple": "#9255C3",
         "light-purple": "#D4BEE4",
-        "bright-green": "#3EE64D",
+        "bright-green": "#228000",
         "bright-red": "#E00F00",
         "white-smoke": "#F8F8F8",
         nero: "#2C2C2C",
