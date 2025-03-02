1ae455d91cdcc7c7315602bb50d9e3cc78a16286
Lasma Poksane <laasmapoksane@gmail.com>
2025-03-03 00:23:50 +0100

    Refactor code to allow themes to be switched via OS settings and manually

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -4,23 +4,42 @@ import { Moon, Sun } from "lucide-react";
 import { useState, useEffect } from "react";
 
 export default function ThemeSwitcher() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState<"light" | "dark" | null>(null);
 
   useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-    if (storedTheme) {
-      setTheme(storedTheme);
-      document.documentElement.classList.add(storedTheme);
-    }
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+      .matches
+      ? "dark"
+      : "light";
+    const storedTheme = localStorage.getItem("theme") as
+      | "light"
+      | "dark"
+      | null;
+
+    setTheme(systemTheme || storedTheme);
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const systemThemeChangeHandler = (event: MediaQueryListEvent) => {
+      setTheme(event.matches ? "dark" : "light");
+    };
+
+    mediaQuery.addEventListener("change", systemThemeChangeHandler);
+
+    return () =>
+      mediaQuery.removeEventListener("change", systemThemeChangeHandler);
   }, []);
 
-  const toggleThemeHandler = () => {
-    const themeSwitch = theme === "light" ? "dark" : "light";
-    setTheme(themeSwitch);
+  useEffect(() => {
+    if (theme) {
+      document.documentElement.classList.remove("light", "dark");
+      document.documentElement.classList.add(theme);
+      localStorage.setItem("theme", theme);
+    }
+  }, [theme]);
 
-    document.documentElement.classList.remove("light", "dark");
-    document.documentElement.classList.add(themeSwitch);
-    localStorage.setItem("theme", themeSwitch);
+  const toggleThemeHandler = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
   return (
