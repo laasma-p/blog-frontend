d2a424476c283fcd28b07b4031ab95270ef36791
Lasma Poksane <laasmapoksane@gmail.com>
2025-03-02 17:28:39 +0100

    Add general styling to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col min-h-dvh bg-white-smoke dark:bg-nero transition duration-300">
         <Navbar />
-        <main>{children}</main>
+        <main className="flex-1 container mx-auto px-4 py-6">{children}</main>
         <Footer />
       </body>
     </html>
