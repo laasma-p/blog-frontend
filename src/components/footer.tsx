2404502734d528ac8549399ec913cbc92b5c218c
Lasma Poksane <laasmapoksane@gmail.com>
2025-03-02 01:21:29 +0100

    Create initial structure of Footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy; {new Date().getFullYear()} Living Bright. All rights reserved.
+      </p>
+    </footer>
+  );
+}
