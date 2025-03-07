d16322424d0a320249b9dce12b757c39eda859ef
Lasma Poksane <laasmapoksane@gmail.com>
2025-03-07 17:35:28 +0100

    Style Footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer>
-      <p>
+    <footer className="bg-chetwode-blue dark:bg-east-side text-white-smoke dark:text-nero py-4">
+      <p className="text-lg font-medium text-center">
         &copy; {new Date().getFullYear()} Living Bright. All rights reserved.
       </p>
     </footer>
