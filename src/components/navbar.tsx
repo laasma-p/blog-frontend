a5305c71d581b94d432a1646f39fd1c734d1ae5d
Lasma Poksane <laasmapoksane@gmail.com>
2025-03-07 13:17:24 +0100

    Fetch categories from database to use as navigation items

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import ThemeSwitcher from "./theme-switcher";
 import Link from "next/link";
 
 export default function Navbar() {
+  const [categories, setCategories] = useState<
+    { name: string; slug: string }[]
+  >([]);
+
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const response = await fetch("http://localhost:3000/categories");
+        const data = await response.json();
+        setCategories(data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    }
+
+    fetchCategories();
+  }, []);
+
   return (
     <header>
       <div>
         <Link href="#">Living Bright</Link>
         <nav>
-          <Link href="#">Tech</Link>
-          <Link href="#">Opinions</Link>
-          <Link href="#">Reviews</Link>
-          <Link href="#">Thoughts</Link>
-          <Link href="#">Lifestyle</Link>
+          {categories.map((category) => {
+            return (
+              <Link key={category.slug} href="#">
+                {category.name}
+              </Link>
+            );
+          })}
         </nav>
         <ThemeSwitcher />
       </div>
